17a955e09814268df16d25c26691e0bc5be9b0d0
Ahmad Tirmizi <105071325+1cecream03@users.noreply.github.com>
2025-06-26 21:00:59 +0800

    Frontend

---
+++ WatchTower/frontend/src/pages/Favorites.jsx
@@ -0,0 +1,9 @@
+import '../css/Favorites.css'
+
+function Favorites() {
+    return <div className="Favorites-empty">
+        <h2>No Favourite Movies Yet</h2>
+    </div>
+}
+
+export default Favorites
---
+++ WatchTower/frontend/src/pages/Home.jsx
@@ -0,0 +1,67 @@
+import MovieCard from "../components/MovieCard"
+import { useState, useEffect } from "react"
+import { searchMovies, getPopularMovies } from "../services/api";
+import '../css/Home.css'
+
+function Home() {
+    const [searchQuery, setSearchQuery] = useState(""); //anytime we update the state, it will rerender and update based on state
+    const [movies, setMovies] = useState([])
+    const [error, setError] = useState(null)
+    const [loading, setLoading] = useState(true)
+
+    useEffect(() => {
+        const loadPopularMovies = async () => {
+            try {
+                const popularMovies = await getPopularMovies()
+                setMovies(popularMovies)
+            } catch (err) {
+                console.log(err)
+                setError("Failed to load movies...")
+            }
+            finally {
+                setLoading(false)
+            }
+        }
+
+        loadPopularMovies()
+    }, [])
+    //function called when dependency array changes
+    //useEffect allow to add side effects to func/ compo and define when they run
+
+    const handleSearch = (e) => {
+        e.preventDefault() //prevent default behaviour, clear
+        alert(searchQuery)
+        setSearchQuery("")
+    };
+
+    return (
+        <div className="home">
+            <form onSubmit={handleSearch} className="search-form">
+                <input
+                    type="text"
+                    placeholder="Search for movies..."
+                    className="search-input"
+                    value={searchQuery}
+                    onChange={(e) => setSearchQuery(e.target.value)} //??this is how u update the state
+                />
+                <button type="submit" className="search-button">
+                    Search
+                </button>
+            </form>
+
+            {error && <div className="error-message">{error}</div>}
+
+            {loading ? (
+                <div className="loading">Loading...</div>
+            ) : (
+                <div className="movies-grid">
+                    {movies.map((movie) => (
+                        <MovieCard movie={movie} key={movie.id} /> //rerun entire component while state remains, return new UI
+                    ))}
+                </div>
+            )}
+        </div>
+    );
+}
+
+export default Home
