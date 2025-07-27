ec745b58f849130b4b31785d3718ce80f040570a
Ahmad Tirmizi <105071325+1cecream03@users.noreply.github.com>
2025-07-28 02:41:55 +0800

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = "django-insecure-nma=xi6x2p-crjg^ifqqkapyu1qjd0l=+wn)-rijk_o%$!k3w_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", "*").split(" ")
 
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
